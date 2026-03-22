05b27bf25e9a9736259f72f378154e26efecc90f
Aditya Gaurav <aditya.gaurav@bold.com>
2026-03-22 16:39:25 +0530

    new files

---
+++ chap02_JS_concept/Let.js
@@ -0,0 +1,17 @@
+
+
+
+let a = 10;
+console.log(a);
+function test() {
+     let a=20;
+    console.log(a);
+    if (true) {
+     let   a=30;
+        console.log(a)
+    }
+    console.log(a);
+}
+
+test();
+console.log(a);
---
+++ chap02_JS_concept/var.js
@@ -0,0 +1,18 @@
+
+
+
+var a = 10;
+console.log(a);
+function test() {
+     var a=20;
+    console.log(a);
+    if (true) {
+     var   a=30;
+        console.log(a)
+    }
+    console.log(a);
+}
+
+test();
+var a= 40;
+console.log(a);
